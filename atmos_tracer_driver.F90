10ded7a06c43ddc490f856cbd2ac40085bde5498
Flexible Modeling System <Flexible.Modeling.System@noaa.gov>
2006-07-31 00:00:00 -0400

    Import of memphis_2006_07.

--- atmos_tracer_driver.F90
+++ atmos_tracer_driver.F90
@@ -113,7 +113,7 @@ logical :: module_is_initialized = .FALSE.
 
 !---- version number -----
 character(len=128) :: version = '$Id: atmos_tracer_driver.F90,v 13.0 2006/03/28 21:07:12 fms Exp $'
-character(len=128) :: tagname = '$Name: memphis $'
+character(len=128) :: tagname = '$Name: memphis_2006_07 $'
 !-----------------------------------------------------------------------
 
 contains
