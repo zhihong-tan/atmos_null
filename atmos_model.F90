b15525434a7032b361f5323dba7fc2f1ddf1a164
Flexible Modeling System <Flexible.Modeling.System@noaa.gov>
2006-08-31 00:00:00 -0400

    Import of memphis_2006_08.

--- atmos_model.F90
+++ atmos_model.F90
@@ -121,7 +121,7 @@ end type ice_atmos_boundary_type
 !-----------------------------------------------------------------------
 
 character(len=128) :: version = '$Id: atmos_model.F90,v 13.0 2006/03/28 21:07:10 fms Exp $'
-character(len=128) :: tagname = '$Name: memphis_2006_07 $'
+character(len=128) :: tagname = '$Name: memphis_2006_08 $'
 
 contains
 
